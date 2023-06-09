3ea919412a58785b2775b3a90a3753ab864a8f6e
bamboodia <crum82@gmail.com>
2023-06-09 17:08:36 +0100

    init

---
+++ app/page.js
@@ -0,0 +1,65 @@
+import Image from "next/image";
+
+export default function Home() {
+	const [streamedData, setStreamedData] = useState("");
+
+	const handleChatSubmit = async (e) => {
+		e.preventDefault();
+		setStreamedData("");
+		const formData = new FormData(e.currentTarget);
+		const response = await fetch("api/chat", {
+			method: "POST",
+			body: JSON.stringify({ prompt: formData.get("prompt") }),
+			headers: { "Content-Type": "application/json" },
+		});
+		const reader = response.body.getReader();
+
+		while (true) {
+			const { done, value } = await reader.read();
+			if (done) {
+				break;
+			}
+			const text = new TextDecoder().decode(value);
+			setStreamedData((prevData) => prevData + text);
+		}
+	};
+
+	const handleClearChat = () => {
+		setStreamedData("");
+	};
+	return (
+		<main className="flex max-w-6xl mx-auto items-center justify-center p-24">
+			<div className="flex flex-col gap-12">
+				<h1 className="text-gray-200 font-extrabold text-6xl text-center">bamGPT 👌</h1>
+				<form onSubmit={handleChatSubmit}>
+					<input
+						className="py-2 px-4 rounded-md bg-gray-600 text-white w-full"
+						type="text"
+						placeholder="Ask me something"
+						name="prompt"
+						required
+					/>
+					<div className="flex justify-center gap-4 py-4">
+						<button
+							type="submit"
+							className="py-2 px-4 rounded-md text-sm bg-lime-700 text-white hover:opacity-80 transition-opacity">
+							Send Chat
+						</button>
+						<button
+							type="button"
+							onClick={handleClearChat}
+							className="py-2 px-4 rounded-md text-sm bg-red-700 text-white hover:opacity-80 transition-opacity">
+							Clear Chat
+						</button>
+					</div>
+				</form>
+				{streamedData && (
+					<div>
+						<h3 className="text-2xl text-gray-400">AI Assistant</h3>
+						<p className="text-gray-200 rounded-md bg-gray-700 p-4">{streamedData}</p>
+					</div>
+				)}
+			</div>
+		</main>
+	);
+}
